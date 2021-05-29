6ff4dc6a810766d0655ef4a0c5f4def39c87191b
Ronak-Jethava <ronakjethava1511@gmail.com>
2021-05-29 18:23:14 +0530

    Database Added

---
+++ Farmer_Portal/crop_info/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render,redirect
+from .models import crop
+
+def crop_info(request):
+    if request.method == "POST":
+        name = request.POST['Name']
+        description = request.POST['desc']
+        prize = request.POST['prize']
+        stock = request.POST['stock']
+        photo = request.POST['photo']
+        
+        obj = crop.objects.create(name=name, description=description, prize=prize, stock=stock, photo=photo)
+        obj.save()
+        return redirect('crop_info.html')
+    else :
+        return render(request, 'login.html')
